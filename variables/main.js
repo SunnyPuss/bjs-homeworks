61479146f1d366e6bad214c9f16ee425f8da0002
SunnyPuss <fomhumster@ya.ru>
2019-05-30 00:55:27 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,27 @@
+//task1
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let sum = algebra + geography + physics;
+let numberOfCourseTaughts = 3;
+let arithmeticalMean = sum / numberOfCourseTaughts;
+console.log(arithmeticalMean);
+
+console.log();
+
+//task2
+
+let name = "Voldemar";
+let message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
+
+console.log();
+
+//task3
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
