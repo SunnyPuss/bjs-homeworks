fa36398ceb0edf8635fbe56a260c2250a8b3d8c3
SunnyPuss <fomhumster@ya.ru>
2019-06-10 01:35:21 +0300

    primitive wrap, yo

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,24 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let endDate = +new Date(date);
+    let startDate = +new Date();
+    let monthsInMilSec = endDate - startDate;
+    let n = Math.ceil(monthsInMilSec / 2628000000);
+    let p = parseInt(+percent) / 100;
+    let P = p / 12  
+    let amountPerMonth = (parseInt(+amount) - parseInt(+contribution)) * (P + P / ((Math.pow(1 + P, n) - 1)));
+    let totalAmount = amountPerMonth * n;
+    if (isNaN(+percent)){
+        return `Параметр 'Процентная ставка' содержит неправильное значение '${percent}'`;
+    }
+    if (isNaN(+amount)){
+        return `Параметр 'Общая стоимость' содержит неправильное значение '${amount}'`;
+    }
+    if (isNaN(+contribution)){
+        return `Параметр 'Начальный взнос' содержит неправильное значение '${contribution}'`;
+    }
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +40,13 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (name === "" || "null" || null){
+        name = "Аноним";
+    } else if (name == "undefined" || undefined) {
+        name = "Аноним";
+    }
+
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    console.log(greeting)
+    return greeting;
 }
